1f62d71e91ee83c3da15ef3cf76f84358b31806d
chavezhg <ramch_01@hotmail.com>
2024-10-03 18:33:38 -0600

    modify user table

--- hma-db/dbo/Tables/user.sql
+++ hma-db/dbo/Tables/user.sql
@@ -1,10 +1,8 @@
 ﻿CREATE TABLE [dbo].[user]
 (
-	[id] INT PRIMARY KEY,
-  [test_name] VARCHAR(255),
-  [description] VARCHAR(255),
-  [cost] DECIMAL(10, 2),
-  [duration] VARCHAR(255),
-  [created_at] DATETIME,
-  [updated_at] DATETIME
+	[id] INT PRIMARY KEY IDENTITY(1,1),
+  [email] VARCHAR(255) NOT NULL,
+  [password] VARCHAR(255) NOT NULL,
+  [created_at] DATETIME NOT NULL DEFAULT GETDATE(),
+  [updated_at] DATETIME NULL
 )
